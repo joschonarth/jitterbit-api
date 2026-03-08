b8a414b375dd57f00b70008adf491c962f3af88c
joschonarth <joschonarth@gmail.com>
2026-03-08 10:42:38 -0300

    chore: add global error handler

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,14 @@ import {
   validatorCompiler,
   type ZodTypeProvider,
 } from "fastify-type-provider-zod"
+import z, { ZodError } from "zod"
+import { env } from "./env"
 
-export const app = fastify().withTypeProvider<ZodTypeProvider>()
+export const app = fastify({
+  logger: {
+    level: env.NODE_ENV === "production" ? "info" : "debug",
+  },
+}).withTypeProvider<ZodTypeProvider>()
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
@@ -15,4 +21,16 @@ app.register(fastifyCors)
 
 app.get("/health", () => {
   return "OK"
+})
+
+app.setErrorHandler((error, request, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: "Validation error.", issues: z.treeifyError(error) })
+  }
+
+  request.log.error(error)
+
+  return reply.status(500).send({ message: "Internal server error." })
 })
