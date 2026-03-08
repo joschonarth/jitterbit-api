f08987ec67e819c0c1bf67836404e8ddeaa21a1f
joschonarth <joschonarth@gmail.com>
2026-03-08 19:19:12 -0300

    chore: setup Prisma Client

--- src/env.ts
+++ src/env.ts
@@ -1,8 +1,10 @@
+import "dotenv/config"
 import z from "zod"
 
 const envSchema = z.object({
   NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
   PORT: z.coerce.number().default(3333),
+  DATABASE_URL: z.string(),
 })
 
 const _env = envSchema.safeParse(process.env)
